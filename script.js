32770d14dbb3f26f7718eab08730eedf22f57d9d
njahnavi24 <jahnavinagavalli24@gmail.com>
2026-09-20 22:14:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 //your code here!
+let list = document.getElementById('infi-list')
+const addItems = (count) => {
+	for(let i = 0; i < count; i++){
+		let li = document.createElement('li')
+		li.textContent = "Item" + (list.children.length  + 1)
+		list.appendChild(li)
+	}
+}
+addItems(2)
 
+list.addEventListener("scroll",()=>{
+	if(list.scrollTop + list.clientHeight >= list.scrollHeight){
+		addItems(2)
+	}
+})
